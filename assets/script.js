f124e8a1adc468cf8cbb79d655120f90e0090973
Carole <carole.ibarlucia@sfr.fr>
2023-08-23 15:14:37 +0200

    projet complet

--- assets/script.js
+++ assets/script.js
@@ -23,17 +23,6 @@ const slides = [
 
 let nombre = slides.length;
 
-// let newSlides = slides.map(slides => slides.bulletPoint)
-// console.log(newSlides)
-
-// let bulletElement = document.createElement("p")
-// bulletElement.innerText = slides[0].bulletPoint + slides[1].bulletPoint + slides[2].bulletPoint + slides[3].bulletPoint
-// let bulletParent = document.querySelector("points")
-// bulletParent.appendChild(bulletElement)
-// // bulletElement.classList = ("dot")
-
-// // console.log(newSlides)
-
 let imgElement = document.getElementById("banner-image");
 let baseurl = "./assets/images/slideshow/"; 
 
@@ -44,18 +33,27 @@ let index = 0
 function bulletDot(){
 	for (let i=0; i<slides.length; i++){
 		let ElementDot = document.createElement("span");
-		ElementDot.classList.add("dot")
-		if(i==0){
-			ElementDot.classList.add("dot_selected")
-		}
 		let bulletParent = document.getElementsByClassName("dots")[0]
-		console.log(bulletParent)
 		bulletParent.appendChild(ElementDot)
-	console.log(ElementDot)
+	
 	}
 }
-
 bulletDot()
+
+
+let ElementDot2 = document.querySelectorAll(".dots span")
+for (let index=0; index < ElementDot2.length; index++){
+	ElementDot2[index].classList = ("dot")
+	ElementDot2[0].classList.add("dot_selected")
+}	
+console.log(ElementDot2)
+
+function nextPreviousDot(){
+	if ((ElementDot2[index]).classList = ("dot_selected")){
+		ElementDot2[index].classList.remove("dot_selected")
+		ElementDot2[index].classList.add("dot")
+}
+}
 
 function next(){
 if (index == nombre -1){
@@ -65,9 +63,7 @@ if (index == nombre -1){
 }
 	imgElement.setAttribute("src", baseurl + slides[index].image)
 	textElement.innerHTML = (slides[index].tagLine)
-	
-	
-	// bulletElement.classList.add("dot_selected")
+	ElementDot2[index].classList.add("dot_selected")
 }
 
 function previous(){
@@ -78,20 +74,19 @@ if (index ==0){
 }
 	imgElement.setAttribute("src", baseurl + slides[index].image)
 	textElement.innerHTML = (slides[index].tagLine)
-	// bulletElement = (slides[index].bulletPoint)
-	// bulletElement.classList.add("dot_selected")
+	ElementDot2[index].classList.add("dot_selected")
 }
 
 let precedent = document.getElementById("left");
 precedent.addEventListener("click", function() {
-	
 	console.log("clic sur précédent")
+	nextPreviousDot()
 	previous()
 });
 
 let suivant = document.getElementById("right");
 suivant.addEventListener("click", function() {
-
 	console.log("clic sur >")
+	nextPreviousDot()
 	next()
 });
